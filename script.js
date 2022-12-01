05ba6216ab9841efcb8a095ff5e762c4fa8ef342
Nanami <82437383+Nanami-Hotta@users.noreply.github.com>
2022-12-01 11:36:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-// Smooth Scrool Sample
+$(function() {
 
-$(function(){
+  // Menu Toggle
 
-   $('a[href^="#"]').click(function() {
+  $('#MenuButton').on('click', function () {
+    $(this).toggleClass('open');
+    $('#MenuList').slideToggle();
+  });
 
-      // 初期設定：移動時間(ms)と頭出し位置
-      var speed = 500;
-      var offset = -0;
+  $('#MenuList a').on('click', function () {
+    if( window.innerWidth <= 768){
+      $('#MenuButton').removeClass('open');
+      $('#MenuList').slideToggle();
+    }
+  });
 
-      // アンカーを取得
-      var anchor = $(this).attr("href");
+  // Window Resize Function
 
-      // ターゲットの位置を取得
-      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
-      var position = target.offset().top + offset;
+  $(window).resize(function(){
 
-      // スクロール（アニメーション）
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
+    if( window.innerWidth <= 768){
 
-      return false;
+      $('#MenuList').hide();
+      $('#MenuButton').show();
+      $('#MenuButton').removeClass('open');
+
+    } else {
+
+      $('#MenuButton').hide();
+      $('#MenuList').show();
+
+    }
+
+  });
+
+
+  // Smooth Scrool
+
+  $('a[href^="#"]').click(function() {
+
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 500;
+    var offset = -96;
+
+    // アンカーを取得
+    var anchor = $(this).attr("href");
+
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
+
+    // スクロール（アニメーション）
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+    return false;
 
    });
 
